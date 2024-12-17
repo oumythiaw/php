153fdca13695e004f1b35044359b18c080d9e811
oumy thiaw <thiawoumy98@gmail.com>
2024-12-17 18:54:25 +0000

    Delete gestion directory

--- gestion/view/categorie/add.php
+++
@@ -1,8 +0,0 @@
-<form action="?controller=categorie&&action=save" method="POST">
-    <label for="">Libelle</label>
-    <input type="text" name="libelle"><br>
-   
-
-    <button type="submit">Ajouter</button>
-
-</form>
